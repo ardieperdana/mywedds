733a782417a27317371708915824110ed96fe4b1
ahmadfauzi <ahmad@quintal.id>
2024-11-27 10:22:22 +0700

    awd

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -1,6 +1,8 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-<script src="https://www.gstatic.com/firebasejs/9.1.3/firebase-app.js"></script>
-<script src="https://www.gstatic.com/firebasejs/9.1.3/firebase-firestore.js"></script>
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
@@ -14,43 +16,40 @@ measurementId: "G-S9V6GYD8PE"
 };
 
 // Initialize Firebase
-const app = firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
+const app = initializeApp(firebaseConfig);
+const db = getDatabase();
+
 
 $(function(){
 	$('#attendanceForm').submit(function(e){
-        event.preventDefault();
-    
-        // Ambil data dari form
-        const name = $('#name').val();
-        const comment = $('#comment').val();
-        
-        // Simpan data ke Firestore
-        db.collection('attendance').add({
-            name: name,
-            comment: comment,
-        }).then(() => {
-            alert("Data submitted successfully!");
-            // Clear form
-            $('#attendanceForm').reset();
-        })
-        .catch(error => {
-            alert("Error submitting data: " + error);
-        });
-      });
-      
-      db.collection('attendance').orderBy('desc').onSnapshot(snapshot => {
-      const commentsDiv = $('#comments');
-      commentsDiv.innerHTML = ''; // Clear previous comments
-      
-      snapshot.forEach(doc => {
-        const data = doc.data();
-        const commentElement = document.createElement('div');
+	    event.preventDefault();
 
-        commentElement.innerHTML = `
-          <p><strong>${data.name}</strong>: ${data.comment}</p>
-        `;
-        commentsDiv.appendChild(commentElement);
-      });
+	    // Ambil data dari form
+	    const name = $('#name').val();
+	    const comment = $('#comment').val();
+	    
+	    // Simpan data ke Firestore
+	    db.ref("attendance").set({
+		  name: name,
+		  comment: comment  
+		}).then(() => {
+	        alert("Data submitted successfully!");
+	        // Clear form
+	        $('#attendanceForm').reset();
+	    }).catch(error => {
+	        alert("Error submitting data: " + error);
+	    });
+      
+	    db.collection('attendance').orderBy('desc').onSnapshot(snapshot => {
+    		const commentsDiv = $('#comments');
+    		commentsDiv.innerHTML = ''; // Clear previous comments
+    		snapshot.forEach(doc => {
+    			const data = doc.data();
+    			const commentElement = document.createElement('div');
+    			commentElement.innerHTML = `
+    			<p><strong>${data.name}</strong>: ${data.comment}</p>
+    			`;
+    		commentsDiv.appendChild(commentElement);
+    	});
     })
 })
