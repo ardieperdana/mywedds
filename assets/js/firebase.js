5b1ed799171bb2fa13048a3909b9fbf1db45a1ee
ahmadfauzi <ahmad@quintal.id>
2024-11-27 10:06:00 +0700

    we

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -1,8 +1,3 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
@@ -16,7 +11,7 @@ measurementId: "G-S9V6GYD8PE"
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 
 $(function(){
